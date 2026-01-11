b84f067763717b666c477f49b144cf6681c3f839
poly <99827144+Polymorqhism@users.noreply.github.com>
2026-01-11 13:35:03 +0530

    rename

--- compile.sh
+++ compile.sh
@@ -1,10 +1,10 @@
 nasm -felf32 boot.s -o boot.o
 nasm -felf32 kernel.asm -o kernel.o
 i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
-i686-elf-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
-grub-file --is-x86-multiboot myos.bin
+i686-elf-gcc -T linker.ld -o spoolyos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
+grub-file --is-x86-multiboot spoolyos.bin
 echo $?
 mkdir -p isodir/boot/grub
-cp myos.bin isodir/boot/myos.bin
+cp myos.bin isodir/boot/spoolyos.bin
 cp grub.cfg isodir/boot/grub/grub.cfg
-grub-mkrescue -o myos.iso isodir
+grub-mkrescue -o spoolyos.iso isodir
